16709f94901deb064e574a7f203ed3333a977048
fm39hz-laptop <hitpoint2k3@gmail.com>
2025-05-24 17:29:58 +0700

    feat(birdkiller): add body exited signal

--- src/Component/BirdKillerComponent/BirdKillerComponent.cs
+++ src/Component/BirdKillerComponent/BirdKillerComponent.cs
@@ -13,13 +13,25 @@ public interface IBirdKillerComponent : IArea2D;
 [Meta(typeof(IAutoNode))]
 public partial class BirdKillerComponent : Area2D, IBirdKillerComponent {
 
-	public override void _Ready() => BodyEntered += OnBodyEntered;
+	public override void _Ready() {
+		BodyEntered += OnBodyEntered;
+		BodyExited += OnBodyExited;
+	}
 
 	private static void OnBodyEntered(Node2D body) {
 		if (body is not IBirdEntity bird) {
 			return;
 		}
+
 		bird.Collide();
+	}
+
+	private static void OnBodyExited(Node2D body) {
+		if (body is not IBirdEntity bird) {
+			return;
+		}
+		GD.Print("shit");
+		//TODO: Tạo gửi signal bird đã tèo
 	}
 
 	#region AutoInject
